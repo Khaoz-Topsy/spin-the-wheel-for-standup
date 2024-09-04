42dd2bdd257b6e0b8e7d94acead7e500a583f454
Lenni009 <guentherhanslphdyt@gmail.com>
2024-09-04 09:39:02 +0200

    Revert "add base path to config"
    This reverts commit 846604204edd5885f006c702698be7c761907ae5.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  base: '/spin-the-wheel-for-standup/',
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
